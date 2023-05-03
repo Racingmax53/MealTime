6eec7291ceca13b91fb86e723c3a428194084084
Racingmax53 <30538609+Racingmax53@users.noreply.github.com>
2023-05-02 22:23:54 -0700

    Added Files
    Created “recipe.html”, “createrecipe.html”, “displayrecipe.html”
    Added dummy data into the collection
    Created list of recipes in the collection that contain keywords in the title on the “recipes” page
    Created “createrecipe’s” form and backend connection
    Added functionality to push created recipe into DB

--- mealtime.py
+++ mealtime.py
@@ -17,7 +17,114 @@ app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
 
 
-mydict = { "username": "Max", "password": "123" }
+# Dummy Data
+
+
+# myCredentials = { "username": "Max", 
+#                 "password": "123" }
+
+# ingredient1 = { "_id": 1,
+#                 "name": "Tomato",
+#                 "dietary": "",
+#                 "unit": ""}
+
+# ingredient2 = { "_id": 2,
+#                 "name": "Egg",
+#                 "dietary": "vegetarian",
+#                 "unit": ""}
+
+# ingredient3 = { "_id": 3,
+#                 "name": "Salmon",
+#                 "dietary": "pescatarian",
+#                 "unit": "oz"}
+
+# ingredient4 = { "_id": 4,
+#                 "name": "Kale",
+#                 "dietary": "vegan",
+#                 "unit": "cup"}
+
+# collection = userDB["Ingredients"]
+
+# collection.insert_one(ingredient1)
+# collection.insert_one(ingredient2)
+# collection.insert_one(ingredient3)
+# collection.insert_one(ingredient4)
+
+# recipe1 = {
+#         "_id": 10,
+#         "title": "Tomato Salad",
+#         "description": "A simple and refreshing salad that's perfect for summer.",
+#         "ingredients": [
+#         {
+#             "name": "Tomato",
+#             "quantity": 2,
+#             "unit": "units"
+#         },
+#         {
+#             "name": "Cucumber",
+#             "quantity": 1,
+#             "unit": "units"
+#         },
+#         {
+#             "name": "Olive Oil",
+#             "quantity": 1,
+#             "unit": "tablespoon"
+#         },
+#         {
+#             "name": "Balsamic Vinegar",
+#             "quantity": 1,
+#             "unit": "tablespoon"
+#         }
+#     ],
+#     "instructions": [
+#         "Slice the tomatoes and cucumber into bite-sized pieces.",
+#         "Combine the olive oil and balsamic vinegar in a small bowl and whisk to combine.",
+#         "Drizzle the dressing over the tomato and cucumber, tossing to coat.",
+#         "Serve immediately."
+#     ]
+# }
+
+# recipe2 = {
+#     "_id": 11,
+#     "title": "Chicken Alfredo",
+#     "description": "A classic pasta dish that's creamy and comforting.",
+#     "ingredients": [
+#         {
+#             "name": "Pasta",
+#             "quantity": 8,
+#             "unit": "ounces"
+#         },
+#         {
+#             "name": "Chicken Breast",
+#             "quantity": 2,
+#             "unit": "units"
+#         },
+#         {
+#             "name": "Heavy Cream",
+#             "quantity": 1,
+#             "unit": "cup"
+#         },
+#         {
+#             "name": "Parmesan Cheese",
+#             "quantity": 1,
+#             "unit": "cup"
+#         }
+#     ],
+#     "instructions": [
+#         "Cook the pasta according to package instructions.",
+#         "While the pasta cooks, season the chicken with salt and pepper and cook in a skillet over medium heat until no longer pink.",
+#         "Remove the chicken from the skillet and set aside.",
+#         "In the same skillet, add the heavy cream and Parmesan cheese, whisking constantly until the cheese is melted and the sauce is smooth.",
+#         "Add the cooked chicken back to the skillet and toss to coat in the sauce.",
+#         "Serve the chicken and sauce over the cooked pasta."
+#     ]
+# }
+
+# collection = userDB["recipes"]
+
+# collection.insert_one(recipe1)
+# collection.insert_one(recipe2)
+
 
 def checkCredentials(_fromDB, _username, _password):
     # Check if the password provided is correct
@@ -31,14 +138,21 @@ def checkCredentials(_fromDB, _username, _password):
         return False
 
 
+# @app.route('/')
+# def home():
+#     if 'logged_in' in session and session['logged_in']:
+#         # User is logged in
+#         return redirect(url_for("user", usr=user))
+#     else:
+#         # User is not logged in
+#         return redirect(url_for("login"))
+
 @app.route('/')
 def home():
-    if 'logged_in' in session and session['logged_in']:
-        # User is logged in
-        return redirect(url_for("user", usr=user))
-    else:
-        # User is not logged in
-        return redirect(url_for("login"))
+    collection = userDB["recipes"]
+    recipes = collection.find()
+    return render_template('home.html', recipes=recipes)
+
 
 
 @app.route('/checkping')
@@ -83,13 +197,18 @@ def login():
 def signup():
     return render_template("createacct.html")
 
-@app.route("/<usr>")
-def user(usr):
-    return render_template("home.html", username=usr)
-
 @app.route("/recipes")
 def recipes():
     return render_template("recipes.html")
+
+@app.route("/search")
+def search():
+    query = request.args.get("q")
+    if not query:
+        return redirect(url_for("recipes"))
+    recipes = userDB.recipes.find({"title": {"$regex": query, "$options": "i"}})
+    return render_template("recipes.html", recipes=recipes)
+
 
 @app.route("/logout")
 def logout():
@@ -99,6 +218,58 @@ def logout():
 @app.route("/displayrecipe")
 def displayrecipe():
     return render_template("displayrecipe.html", title="Test")
+
+
+@app.route('/createrecipe', methods=['GET', 'POST'])
+def show_createrecipe():
+    if request.method == 'POST':
+        name = request.form['name']
+        description = request.form['description']
+        ingredients = request.form['ingredients']
+        steps = request.form['steps']
+
+        # Store the recipe data in a database
+        _recipe = {
+            "_id": 1,
+            "title": name,
+            "description": description,
+            "ingredients": ingredients,
+            "instructions": steps
+        }
+
+        collection = userDB["recipes"]
+        collection.insert_one(_recipe)
+
+        return redirect(url_for('show_createrecipe'))
+
+    return render_template('createrecipe.html')
+
+
+
+class Recipe:
+    def __init__(self, recipe_id, name, description, ingredients, steps):
+        self.recipe_id = recipe_id
+        self.name = name
+        self.description = description
+        self.ingredients = ingredients
+        self.steps = steps
+
+# route for displaying a recipe
+@app.route('/displayrecipe/<int:recipe_id>')
+def display_recipe(recipe_id):
+    collection = userDB["recipes"]
+    # retrieve the recipe from the database
+    recipe = collection.find_one({'_id': recipe_id})
+
+    # create a Recipe object
+    name = recipe['title']
+    description = recipe['description']
+    ingredients = recipe['ingredients']
+    steps = recipe['instructions']
+    recipe_obj = Recipe(recipe_id, name, description, ingredients, steps)
+
+    # pass the Recipe object to the template
+    return render_template('displayrecipe.html', recipe=recipe_obj)
 
 if __name__ == '__main__': 
     app.run(debug=True)
